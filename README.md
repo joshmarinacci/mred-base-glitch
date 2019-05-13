571a63fab2744053d66e516844681e1a01beb01b
Glitch (buttered-elk) <none>
2019-05-13 16:47:43 +0000

    💸🎃 Updated with Glitch

---
+++ README.md
@@ -0,0 +1,39 @@
+This is an XR editor. The page has two buttons, one to view your creations and one to open
+the editor. Any scenes you create will be yours to keep.  The actual editor is hosted
+on vr.josh.earth but all your documents are kept here on this glitch.
+
+foo
+bar
+
+
+
+
+
+
+Welcome to Glitch
+=================
+
+Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
+
+**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
+
+Find out more [about Glitch](https://glitch.com/about).
+
+
+Your Project
+------------
+
+On the front-end,
+- edit `public/client.js`, `public/style.css` and `views/index.html`
+- drag in `assets`, like images or music, to add them to your project
+
+On the back-end,
+- your app starts at `server.js`
+- add frameworks and packages in `package.json`
+- safely store app secrets in `.env` (nobody can see this but you and people you invite)
+
+
+Made by [Glitch](https://glitch.com/)
+-------------------
+
+\ ゜o゜)ノ
---
+++ public/.build/precache-manifest.e531edb2390dc13299feaa1f2123cc32.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b17311f2333e8fda8b05",
+    "url": "./static/css/main.e68e3ab6.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f58bf135cfad40e2b37e",
+    "url": "./static/js/1.f58bf135.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b17311f2333e8fda8b05",
+    "url": "./static/js/main.b17311f2.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "f58bf135cfad40e2b37e",
+    "url": "./static/css/1.1516d0f8.chunk.css"
+  },
+  {
+    "revision": "d7e9ad0ee8cb4edf42281b4db295b07a",
+    "url": "./index.html"
+  }
+];
