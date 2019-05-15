9cb63bc7756e64fa4b218a674061be0116d3a89c
Blair MacIntyre <bmacintyre@mozilla.com>
2019-05-15 11:36:51 -0400

    fix

--- public/.build/service-worker.js
+++ public/.build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e30cd9d380e0ef8c75fd57fe2a54bcd2.js"
+  "./precache-manifest.af200e326319a02a0948cd4785bc76c7.js"
 );
 
 workbox.clientsClaim();
