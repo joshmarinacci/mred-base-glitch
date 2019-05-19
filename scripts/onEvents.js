510cf55a0bad6bd6e62882e85675e1747513f6e0
Glitch (blairmacintyre-mred-base-glitch) <none>
2019-05-19 19:14:03 +0000

    🗾🌄 Checkpoint ./docs/doc_duck_quack.json:273376/34 ./docs/doc_duck_image.json:273376/19 ./docs/doc_duck_geo.json:273376/33 ./docs/doc_duck.json:273376/35 ./scripts/onEvents.js:273376/5449

---
+++ scripts/onEvents.js
@@ -0,0 +1,102 @@
+/*
+#title onEvents
+#description runs the provided code in the common event handlers
+*/
+({
+    properties: {
+        start: {
+            type:'string',
+            title: "when project starts:",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        },
+        stop: {
+            type:'string',
+            title: "when project stop:",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        },
+        enter: {
+            type:'string',
+            title: "when scene is entered:",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        },
+        exit: {
+            type:'string',
+            title: "when scene is exited:",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        },
+        tick: {
+            type:'string',
+            title: "on clock tick:",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        },
+        message: {
+            type:'string',
+            title: "when a message is received:",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        }
+    },
+
+    start: function (e) {      
+      let props = this.properties
+      
+      let start = "function(e) { ((e) => {" + props.start + "}).call(this, e)}"
+      this.startFunction = Function('"use strict";return (' + props.start + ')')()
+      
+      let stop = "function(e) { ((e) => {" + props.stop + "}).call(this, e)}"
+      this.stopFunction = Function('"use strict";return (' + props.stop + ')')()
+
+      let enter = "function(e) { ((e) => {" + props.enter + "}).call(this, e)}"
+      this.enterFunction = Function('"use strict";return (' + props.enter + ')')()
+
+      let exit = "function(e) { ((e) => {" + props.exit + "}).call(this, e)}"
+      this.exitFunction = Function('"use strict";return (' + props.exit + ')')()
+
+      let message = "function(e) { ((e) => {" + props.message + "}).call(this, e)}"
+      this.messageFunction = Function('"use strict";return (' + props.message + ')')()
+
+      let tick = "function(e) { ((e) => {" + props.tick + "}).call(this, e)}"
+      this.tickFunction = Function('"use strict";return (' + props.tick + ')')()
+
+      // and run the start method!
+      this.startFunction.call(this, e)
+    },
+  
+    stop: function(e) {
+      this.stopFunction.call(this, e)      
+    },
+  
+    click: function(e) {
+      this.clickFunction.call(this, e)      
+    },
+  
+    click: function(e) {
+      this.clickFunction.call(this, e)      
+    },
+  
+    click: function(e) {
+      this.clickFunction.call(this, e)      
+    },
+  
+    click: function(e) {
+      this.clickFunction.call(this, e)      
+    }
+  
+})
