2b74910bfe45c94f68b52ea28792839cee906e08
Glitch (blairmacintyre-mred-base-glitch) <none>
2019-05-19 12:12:09 +0000

    🌕🚔 Checkpoint ./scripts/onclick.js:273376/137

--- scripts/onclick.js
+++ scripts/onclick.js
@@ -18,16 +18,17 @@
     start: function (e) {
       this.logger.log("looking for THREE?")
       let Three = window.THREE
-      this.logger.log("did we find THREE?", Three)
+      this.logger.log("did we find THREE? ", typeof(Three))
       
       let snippet = this.properties.code
-      let code = "function(this,e) {" + snippet + "}"
-    this.logger.log ("click function: ", code)
+      let code = "function(e) { call((e) => {" + snippet + "}, this)}"
+      this.logger.log ("click function: ", code)
       this.clickFunction = Function('"use strict";return (' + code + ')')()
     this.logger.log("set click function")
     },
   
     click: function(e) {
-      this.clickFunction(this,e)      
+      let that = this
+      call(this.clickFunction(e), this)      
     }
 })
