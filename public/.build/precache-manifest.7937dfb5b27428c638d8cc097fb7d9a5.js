8683f8192a014740274d43eb4396baaf5672814d
Blair MacIntyre <bmacintyre@mozilla.com>
2019-05-19 12:39:46 -0400

    update

---
+++ public/.build/precache-manifest.7937dfb5b27428c638d8cc097fb7d9a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "6b9d438dabf5db3b5be9",
+    "url": "./static/css/main.0033c942.chunk.css"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3128a7459250210889fe",
+    "url": "./static/js/1.3128a745.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6b9d438dabf5db3b5be9",
+    "url": "./static/js/main.6b9d438d.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "3128a7459250210889fe",
+    "url": "./static/css/1.50a1c615.chunk.css"
+  },
+  {
+    "revision": "e3adffd40a8026ae4fb6f24b0f8db44f",
+    "url": "./index.html"
+  }
+];
