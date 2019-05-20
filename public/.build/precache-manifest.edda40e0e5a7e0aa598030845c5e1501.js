79e951c90a2697983647a81c03d10df07afe22ad
Anselm Hook <anselm@hook.org>
2019-05-20 15:07:29 -0700

    updated build

---
+++ public/.build/precache-manifest.edda40e0e5a7e0aa598030845c5e1501.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "367b12bff598e978f2d7",
+    "url": "./static/css/main.bea39b31.chunk.css"
+  },
+  {
+    "revision": "367b12bff598e978f2d7",
+    "url": "./static/js/main.367b12bf.chunk.js"
+  },
+  {
+    "revision": "2af284f17ff9da15b85d",
+    "url": "./static/css/1.4ceeeb70.chunk.css"
+  },
+  {
+    "revision": "2af284f17ff9da15b85d",
+    "url": "./static/js/1.2af284f1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1e9e8de048ce813cdcbcf5f8241c8963",
+    "url": "./index.html"
+  }
+];
--- public/.build/service-worker.js
+++ public/.build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0f9f2fa9006cc4420ce0966be54c6b34.js"
+  "./precache-manifest.edda40e0e5a7e0aa598030845c5e1501.js"
 );
 
 workbox.clientsClaim();
