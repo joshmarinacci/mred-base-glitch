ee00f594aeef46783852e6f298f70273ce93faba
Blair MacIntyre <bmacintyre@mozilla.com>
2019-05-27 23:42:16 +0000

    🌓👩‍❤️‍💋‍👩 Checkpoint ./scripts/onEvent.js:273376/639 ./scripts/showIf.js:273376/1682

--- scripts/onEvent.js
+++ scripts/onEvent.js
@@ -1,6 +1,6 @@
 /*
-#title onEvents
-#description Runs the provided code in the common event handlers
+#title onEvent
+#description Runs the provided code in the specified event handler
 */
 ({
     properties: {
@@ -23,34 +23,12 @@
       let props = this.properties
       
       let code = "function(e) { ((e) => {" + props.code + "}).call(this, e)}"
-      this.theFunction = Function('"use strict";return (' + code + ')')()
+      let theFunction = Function('"use strict";return (' + code + ')')()
       
       if (code.hasOwnProperty(props.event)) {
         this.logger.error("onEvent script tried to overwrite event handler for " + props.event)
       } else {
-        
-        // and run the start method!
-      this.startFunction.call(this, e)
-    },
-  
-    stop: function(e) {
-      this.stopFunction.call(this, e)      
-    },
-  
-    enter: function(e) {
-      this.enterFunction.call(this, e)      
-    },
-  
-    exit: function(e) {
-      this.exitFunction.call(this, e)      
-    },
-  
-    message: function(e) {
-      this.messageFunction.call(this, e)      
-    },
-  
-    tick: function(e) {
-      this.tickFunction.call(this, e)      
+        this.code[props.event] = theFunction
+      }
     }
-  
 })
---
+++ scripts/showIf.js
@@ -0,0 +1,28 @@
+/*
+#title showIf
+#description if the provide test evauates to true, the node is visible
+*/
+({
+    properties: {
+        conditional: {
+            type:'string',
+            title: "test to decide if the node is shown",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        }
+    },
+
+    start: function (e) {      
+      let props = this.properties
+      
+      let code = "function(e) { if (" + props.conditional + ") { e.target.visible = true } else { e.target.visible = false } }"
+      this.tickFunction = Function('"use strict";return (' + code + ')')()
+      
+    },
+  
+    tick: function(e) {
+      this.tickFunction.call(this, e)      
+    }
+})
