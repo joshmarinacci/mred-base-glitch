a5d44d2c8e4472c1bb2247bab96f592d924af2c9
Blair MacIntyre <bmacintyre@mozilla.com>
2019-05-28 10:20:05 +0000

    🏉🐑 Checkpoint

--- scripts/setText.js
+++ scripts/setText.js
@@ -19,8 +19,6 @@
       
       let code = "function(e) { return ((e) => {" + props.code + "}).call(this, e)}"
       this.tickFunction = Function('"use strict";return (' + code + ')')()
-
-      props.initialText = e.target.userData.div.innerHTML
     },
   
     tick: function(e) {
