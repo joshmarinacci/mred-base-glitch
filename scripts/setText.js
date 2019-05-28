adc35b350f3b16132ecaca7713d8d1a9f9db66d7
Blair MacIntyre <bmacintyre@mozilla.com>
2019-05-28 00:04:15 +0000

    🍰📷 Checkpoint ./scripts/setText.js:273376/1406

---
+++ scripts/setText.js
@@ -0,0 +1,28 @@
+/*
+#title setText
+#description the provided code should return a text string, which will be set to the text object
+*/
+({
+    properties: {
+        code: {
+            type:'string',
+            title: "compute the text to display",
+            value: "",
+            hints: {
+                multiline:true,
+            }
+        }
+    },
+
+    start: function (e) {      
+      let props = this.properties
+      
+      let code = "function(e) { return (" + props.code + ") }"
+      this.tickFunction = Function('"use strict";return (' + code + ')')()
+      
+    },
+  
+    tick: function(e) {
+      let text = this.tickFunction.call(this, e)      
+    }
+})
