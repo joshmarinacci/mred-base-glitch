5abab91d1bad223a0b686060e2c04de19fd02dba
Blair MacIntyre <bmacintyre@mozilla.com>
2019-05-30 09:52:14 +0000

    🐺👆 Checkpoint

--- scripts/infrontof.js
+++ scripts/infrontof.js
@@ -11,7 +11,11 @@
         delay: {
             type:'number',
             value: 2000,
-        },    
+        }, 
+        alpha: {
+            type: 'number',
+            value: 0.1
+        }
         // duration: {
         //     type:'number',
         //     value: 0,
@@ -20,57 +24,32 @@
     start: function (event) {
 
         let THREE = this.globals.THREE
+        
         this.distance = this.properties.distance
         this.delay = this.properties.delay
-//         this.duration = this.properties.duration
+        this.alpha = this.properties.alpha
         this.idealPos = new THREE.Vector3()
-        this.scratch = new THREE.Matrix4()
-//         this.latchDone = false
-//         if(!this.properties.distance) {
-//             let target = event.target
-//             let camera = this.camera
-//             let targetPos = new THREE.Vector3()
-            this.cameraPos = new THREE.Vector3()
-//             target.getWorldDirection(targetPos)
-//             camera.getWorldDirection(cameraPos)
-//             this.distance = targetPos.distanceTo(cameraPos)
-//         }
+        this.cameraPos = new THREE.Vector3()
     },
     enter: function(event) {
-        this.startTime = -this.delay - 1   // ensure it runs first tick
+        this.startTime = this.time
+        
+        this.camera.getWorldDirection(this.cameraPos)
+        this.cameraPos.normalize()
+        this.cameraPos.multiplyScalar(distance)
+        event.target.position.copy(this.cameraPos)
+        this.idealPos.copy(this.cameraPos)        
     },
     tick: function(event) {
-        if (this.startTime + this.delay <= event.time)
+        if (this.delay > 0 && this.startTime + this.delay <= event.time) {
             this.startTime = event.time
 
             // distance
             this.camera.getWorldDirection(this.cameraPos)
-            this.logger.log("in front of: direction ", this.cameraPos)
             this.cameraPos.normalize()
             this.cameraPos.multiplyScalar(distance)
-            this.logger.log("in front of: val ", this.cameraPos)
-            this.logger.log("in front of: event position ", event.target.position)
-            event.target.position.copy(this.cameraPos)
-      this.logger.log("in front of: event position ", event.target.position)
-//     	let speed = this.speed
-//     	let idealPos = this.idealPos
-//     	let target = event.target
-//     	let camera = this.camera
-
-//         // ideal
-//       	idealPos.set(0,0,-dist)
-//         idealPos.applyQuaternion(camera.quaternion)
-        //idealPos.add(camera.position)
-
-        // if(speed) {
-        //     target.position.set(
-        //         target.position.x + (idealPos.x-target.position.x)/2 * speed,
-        //         target.position.y + (idealPos.y-target.position.y)/2 * speed,
-        //         target.position.z + (idealPos.z-target.position.z)/2 * speed
-        //         )
-        // } else {
-        //     target.position.set(idealPos.x,idealPos.y,idealPos.z)
-        // }
-
+            this.idealPos.copy(this.cameraPos)
+        }
+        event.target.position.lerp(this.idealPos, this.alpha)
     }
   })
